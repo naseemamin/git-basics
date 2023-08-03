8d3d1528605cd8d8283a6adfa529eb1c8138a5cb
naseemamin <naseem.amin@capgemini.com>
2023-08-03 13:17:03 +0100

    adding python file

---
+++ Demo.py
@@ -0,0 +1,24 @@
+#Demo python file
+chosen_numbers = [1,3,5,7,9,11]
+
+squared_numbers = []
+
+ 
+
+def square_nums_func():
+
+ for x in chosen_numbers:
+
+   number =  x * x
+
+   squared_numbers.append(number)
+
+   squared_numbers.sort()
+
+   print(squared_numbers)
+
+ 
+
+ 
+
+square_nums_func()
